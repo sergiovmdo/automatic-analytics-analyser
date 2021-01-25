97cdc4f5a00c12d38a40422aad53e3c1b453e24b
sergiovmdo97 <sergiovazquezmontesdeoca@gmail.com>
2021-01-25 10:23:05 +0100

    Added texts to main activity

--- app/src/main/java/view/MainActivity.kt
+++ app/src/main/java/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.automatic_analytics_analyser
+package view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.automatic_analytics_analyser.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
