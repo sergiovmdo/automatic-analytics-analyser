fbbcc523c8cedea872f5994167f2497c8cd6d434
sergiovmdo97 <sergiovazquezmontesdeoca@gmail.com>
2021-03-24 12:13:12 +0100

    Changes in endpoints names.

--- app/src/main/java/com/example/automatic_analytics_analyser/data/api/ApiService.kt
+++ app/src/main/java/com/example/automatic_analytics_analyser/data/api/ApiService.kt
@@ -3,9 +3,13 @@ package com.example.automatic_analytics_analyser.data.api
 import com.example.automatic_analytics_analyser.model.UserBuilder
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface ApiService {
-    @POST("user")
+    @POST("register")
     suspend fun newUser(@Body user: UserBuilder): Response<Unit>
+
+    @GET("login")
+    suspend fun getUser(): Response<UserBuilder>
 }
